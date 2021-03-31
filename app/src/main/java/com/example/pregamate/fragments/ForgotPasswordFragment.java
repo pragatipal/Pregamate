4f26d74088a6933fd7b764dc22246027cec86e11
Pragati Pal <juhipal16@gmail.com>
2021-03-31 23:12:51 +0530

    more changes

--- app/src/main/java/com/example/pregamate/fragments/ForgotPasswordFragment.java
+++ app/src/main/java/com/example/pregamate/fragments/ForgotPasswordFragment.java
@@ -4,13 +4,21 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.pregamate.R;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ForgotPasswordFragment extends Fragment {
 
+    private Button recoverBtn;
+    private EditText emailEt3;
+    private FirebaseAuth auth;
     public ForgotPasswordFragment() {
         // Required empty public constructor
     }
@@ -27,4 +35,33 @@ public class ForgotPasswordFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false);
     }
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        init(view);
+        clickListener();
+    }
+
+    private void clickListener() {
+        recoverBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email= emailEt3.getText().toString();
+                String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+                if (email.isEmpty() || !email.matches(emailPattern)){
+                    emailEt3.setError("Please inpput valid email.");
+                }
+                auth.sendPasswordResetEmail(email);
+            }
+        });
+    }
+
+
+    private void init(View view) {
+        auth = FirebaseAuth.getInstance();
+        recoverBtn = getView().findViewById(R.id.startBtn);
+        emailEt3 = getView().findViewById(R.id.emailEt3);
+    }
+
+
 }
--- app/src/main/java/com/example/pregamate/fragments/WelcomeFragment.java
+++ app/src/main/java/com/example/pregamate/fragments/WelcomeFragment.java
@@ -1,17 +1,30 @@
 package com.example.pregamate.fragments;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import com.example.pregamate.R;
 
+import java.time.LocalDate;
+
 
 public class WelcomeFragment extends Fragment {
 
+    private CalendarView calendarView;
+    private static final String TAG = "CalendarActivity";
+    private int trimsPregnant;
+    private long diffYear, diffMonth;
+    private Button startButton;
 
     public WelcomeFragment() {
         // Required empty public constructor
@@ -22,5 +35,53 @@ public class WelcomeFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_welcome, container, false);
+    }
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        init();
+        trimsPregnant =getTimePregnant();
+        letsStart();
+    }
+
+    private void letsStart() {
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+//    private void moveToHomeActivity(int trimsPregnant) {
+//
+//    }
+
+    private int getTimePregnant() {
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                LocalDate date1 = LocalDate.now();
+                diffYear = date1.getYear()-year;
+                if (date1.getYear()!=year){
+                    diffMonth = 12- date1.getMonthValue()+month;
+                }
+                else {
+                   diffMonth = date1.getMonthValue()-month;
+                }
+            }
+        });
+        return (int) ((diffYear*12+diffMonth));
+    }
+
+
+
+
+    private void init() {
+        calendarView = getView().findViewById(R.id.calendarView);
+        startButton = getView().findViewById(R.id.startBtn);
+
     }
 }
