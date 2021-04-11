d1ea3de7cbce3ed5ea8f3874a30f0e633ced9ce6
Pragati Pal <juhipal16@gmail.com>
2021-04-11 21:48:01 +0530

    updates

--- app/src/main/java/com/example/pregamate/ui/user/UserFragment.java
+++ app/src/main/java/com/example/pregamate/ui/user/UserFragment.java
@@ -4,32 +4,46 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.example.pregamate.R;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class UserFragment extends Fragment {
 
-    private UserViewModel userViewModel;
+//    Firebase
+    private FirebaseAuth auth;
+    private FirebaseUser user;
+    private FirebaseDatabase db;
+    private DatabaseReference ref;
+
+
+//    Views from xml
+    private ImageView userProfileIv;
+    private TextView nameTv, emailTv, trimesterTv;
+
+    public UserFragment(){
+//       Required public constructor
+    }
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        userViewModel =
-                new ViewModelProvider(this).get(UserViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_user, container, false);
-        final TextView textView = root.findViewById(R.id.text_user);
-        userViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+
+//        init firebase
+        auth =FirebaseAuth.getInstance();
+        user= auth.getCurrentUser();
+        db= FirebaseDatabase.getInstance();
+        ref= db.getReference();
+
+
+        return inflater.inflate(R.layout.fragment_user, container, false);
     }
 }
